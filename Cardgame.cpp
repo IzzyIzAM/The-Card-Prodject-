b4d9bdfb7d3d31350457e958954f425999924d8d
IzzyIzAM <104239828+IzzyIzAM@users.noreply.github.com>
2022-04-22 20:41:53 -0500

    Add files via upload

---
+++ Cardgame.cpp
@@ -0,0 +1,246 @@
+// Cardgame.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+class card
+{
+public:
+	card() {
+		curcard = 0;
+	}
+	
+	card(int val) {
+		curcard = val;
+	}
+
+	void setCard(card val) { curcard = val.cardType(); }
+
+	void setCard(int val) { curcard = val; }
+
+	int valueofcard() { return value[curcard]; }
+
+	string visual() { return vis[curcard]; }
+
+	int cardType() { return curcard; }
+
+	void print() { cout << "[" << vis[curcard] << "]"; }
+private:
+	
+	string vis[16] = { "-3","A","2","3","4","5","6","7","8","9","10","J","Q","K","JK"," "};
+	int value[16] = { 100,1,2,3,4,5,6,7,8,9,10,10,0,-2,0};
+	int curcard;
+	
+};
+
+class deckOfCards {
+private:
+	card* deck;
+	int numdeck,totalcards,index; //store the number of decks 2 for every 2 players, stors total number of cards, stores the index of the card
+	card discard; // discarded card
+
+	void reset() {
+		index = 0;
+		for (int k = 1; k <= numdeck; k++) { //for the number of decks
+			for (int j = 1; j <= 4; j++) { // for each suit
+				for (int i = 1; i <= 13; i++) { // for each card
+					deck[index].setCard(i);
+					index++;
+				}
+			}
+		}
+
+		for (int k = 1; k <= numdeck; k++) { //for the number of decks
+			for (int j = 1; j <= 2; j++) {
+				deck[index].setCard(14);
+				index++;
+			}
+		}
+
+		index = 0;
+	}
+
+public:
+	deckOfCards(int num = 1) {
+
+		totalcards = 52 * numdeck;
+		deck = new card[totalcards];
+		index = 0;
+
+		//set all cards to default
+		for (int t = 0; t < totalcards; t++) {
+			deck[t].setCard(0);
+		}
+
+		//maksure we have enough and the proper cads
+		reset();
+		randomize();
+
+	}
+
+	void randomize() {
+		for (int i = 0; i < totalcards; i++) {
+			
+			//gets random place to swap with
+			int j = rand() % totalcards;
+					
+			//swap places 
+			int temp = deck[i].cardType();
+			deck[i].setCard(deck[j].cardType());
+			deck[j].setCard(temp);
+		}
+		index = 0;
+	}
+
+	card draw() {
+		int i = index;
+		index++;
+		return deck[i];
+	}
+
+	void setDiscard(card car) {
+		discard.setCard(car);
+	}
+
+	card getDiscard() {
+		return discard;
+	}
+	
+};
+
+class player {
+private:
+	card hand[2][3];
+	bool cardvisable[2][3];
+	bool isPlayer;
+public:
+	player() {
+	
+	}
+
+	void setHand(card arry[6]) {
+		//sets all of the cards to the apropriate cards
+		int index = 0;
+		for (int i = 0; i < 2; i++)
+			for (int j = 0; j < 3; j++) {
+				hand[i][j].setCard(arry[index].cardType());
+				cardvisable[i][j] = false;
+				index++;
+			}
+	}
+
+	void placeCard(card theCard, int r, int c) {
+		hand[r][c] = theCard;
+		cardvisable[r][c] = true;
+	}
+
+	card getCard(int r, int c) {
+		//returns the card if its facing up
+		card ret(15);
+		if (cardvisable[r][c])
+		{
+			return hand[r][c];
+		}
+		return ret;
+	}
+	 
+	void print() {
+		//prints the players hand
+		card ret(15);
+		for (int i = 0; i < 2; i++) {
+			for (int j = 0; j < 3; j++) {
+				if (cardvisable[i][j])
+					hand[i][j].print();
+				else
+					ret.print();
+			}
+			cout << endl;
+		}
+
+	
+	}
+
+};
+
+class board {
+private:
+	deckOfCards TheDeck;
+	player* players;
+	int* scoreboard;
+	int numOfPlayers;
+
+public:
+
+	board(int numOfPlayers) {
+		players = new player[numOfPlayers];
+		this->numOfPlayers = numOfPlayers;
+		scoreboard = new int[numOfPlayers];
+	
+	}
+	
+	void score() {
+	//coint the players scores and draws them
+	
+	};
+	
+	card draw() {
+	//draws the table
+		return TheDeck.draw();
+	};
+
+	void pass() {
+	//draws 
+	//put it in discard
+	};
+
+	void deal() {
+		card arry[6];
+		for (int i = 0; i < numOfPlayers; i++)
+			for (int j = 0; j < 6; j++) {
+				arry[j].setCard(draw());
+				players[i].setHand(arry);
+
+			}
+	}
+
+	card takeFromDeck() {
+	
+	}
+
+	card takeFromDiscard() {
+	
+	}
+
+};
+
+
+
+
+
+int main()
+{
+	bool game = false;
+	srand(time(0));
+    std::cout << "Hello World!\n";
+	
+	while (game)
+	{
+
+	}
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
