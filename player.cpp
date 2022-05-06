df33e73e78f10770843fd19ae0c011f636fb15dd
Kantalope <ThomasHook@my.unt.edu>
2022-05-06 17:51:32 -0500

    whatever

--- player.cpp
+++ player.cpp
@@ -1,55 +1,77 @@
+#include "player.h"
+#include "deck.h"
 #include <iostream>
 using namespace std;
 
-class player {
-private:
-	card hand[2][3];
-	bool cardvisable[2][3];
-	bool isPlayer;
-public:
-	//initializes a empty player
-	player() {
-		
-	}
+player() {
+	
+}
 
-	void setHand(card arry[6]) {
-		//sets all of the cards to the apropriate cards
-		int index = 0;
-		for (int i = 0; i < 2; i++)
-			for (int j = 0; j < 3; j++) {
-				hand[i][j].setCard(arry[index].cardType());
-				cardvisable[i][j] = false;
-				index++;
-			}
+void player::setHand(card arry[6]) {
+//sets all of the cards to the apropriate cards
+int index = 0;
+for (int i = 0; i < 2; i++)
+	for (int j = 0; j < 3; j++) {
+		hand[i][j].setCard(arry[index].cardType());
+		cardvisable[i][j] = false;
+		index++;
 	}
+}
 
-	void placeCard(card theCard, int r, int c) {
-		hand[r][c] = theCard;
-		cardvisable[r][c] = true;
-	}
+void player::placeCard(card theCard, int r, int c) {
+	hand[r][c] = theCard;
+	cardvisable[r][c] = true;
+}
 
-	card getCard(int r, int c) {
-		//returns the card if its facing up
-		card ret(15);
-		if (cardvisable[r][c])
-		{
-			return hand[r][c];
-		}
-		return ret;
+card player::getCard(int r, int c) {
+	//returns the card if its facing up
+	card ret(15);
+	if (cardvisable[r][c])
+	{
+		return hand[r][c];
 	}
+	return ret;
+}
 	 
-	void print() {
-		//prints the players hand
-		card ret(15);
-		for (int i = 0; i < 2; i++) {
-			for (int j = 0; j < 3; j++) {
-				if (cardvisable[i][j])
-					hand[i][j].print();
-				else
-					ret.print();
-			}
-			cout << endl;
+void player::print() {
+	//prints the players hand
+	card ret(15);
+	for (int i = 0; i < 2; i++) {
+		for (int j = 0; j < 3; j++) {
+			if (cardvisable[i][j])
+				hand[i][j].print();
+			else
+				ret.print();
 		}
+		cout << endl;
 	}
+}
 
-};
+void player::swap(int r, int c, card theCard, deck theDeck)
+{
+	// set card to toDiscard, place passed card into hand
+	card toDiscard = hand[r][c];
+	hand[r][c] = theCard;
+
+	// make sure card is flipped rightside-up
+	cardvisible[r][c] = true;
+
+	// discard toDiscard
+	theDeck.setDiscard(toDiscard);
+}
+
+bool flip(int r, int c)
+{
+	// check if the card is face down
+	if (cardvisible[r][c] == false)
+	{
+		// flip it and return true
+		cardvisible[r][c] == true;
+		return true;
+	}
+	else
+	{
+		// return false, didn't flip
+		return false;
+	}
+}
--- player.h
+++ player.h
@@ -1,1 +1,18 @@
+#pragma once
+using namespace std;
 
+class player {
+    private:
+	card hand[2][3];
+	bool cardvisable[2][3];
+	bool isPlayer;
+public:
+	//initializes a empty player
+	player();
+    void setHand(card arry[6]);
+    void placeCard(card theCard, int r, int c);
+    card getCard(int r, int c);
+    void print();
+    void swap(int r, int c, card theCard, deck theDeck); // swap drawn card with existing one
+    bool flip(int r, int c); // flip chosen card, return if true if flipped
+}
