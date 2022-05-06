dd5d3fa98d86d79f5acd05d65b29a2897e6f95f6
NightOfDevils <44612725+NightOfDevils@users.noreply.github.com>
2022-05-06 18:11:56 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,10 +7,14 @@ using namespace std;
 int main()
 {
   deckOfCards theDeck;
-  
-  
+  board theBoard;
+  player thePlayer;
+  deck decks;
   
   cout >> "Game Start";
+  
+  
+  
   cout >> "Settiny up deck";
   cout >> "Shuffling deck"
   cout >> "Dealing hands"
