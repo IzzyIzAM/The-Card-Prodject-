b906226cfe32ae249e6f5289534dd3e782ba8014
NightOfDevils <44612725+NightOfDevils@users.noreply.github.com>
2022-05-06 18:42:13 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,17 +7,24 @@ using namespace std;
 int main()
 {
   board theBoard;
-  player playerOne; // Creates Player 1
-  player playerTwo;
+  player thePlayer; // Creates Player 1
   deck decks;
   int currentTurn;
   int playerAmount;
+  int row;
+  int column;
   
   cout << "Game Start";
 
   deckOfCards theDeck; // Creates the Deck  
   
   theBoard.deal(); // Deals cards to both players
+  
+  
+  cout << "Please enter the card you would like to flip in row 1(1-3): ";
+  cin >> column;
+  thePlayer.flip(1, column);
+ 
   
   cout << "Setting up deck";
   cout << "Shuffling deck";
