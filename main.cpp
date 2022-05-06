03b8f10b711a54cb464bc585900ed65766b6c92e
NightOfDevils <44612725+NightOfDevils@users.noreply.github.com>
2022-05-06 18:24:59 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,13 +9,31 @@ int main()
   board theBoard;
   player thePlayer;
   deck decks;
+  int currentTurn;
+  int playerAmount;
   
-  cout >> "Game Start";
-  deckOfCards theDeck;
+  cout << "Game Start";
+
+  deckOfCards theDeck; // Creates the Deck
   
+  cout << "Please enter number of players(2-4): ";
+  cin >> playerAmount;
+ 
+  while(true)
+  {
+      if(playerAmount < 2 || playerAmount > 4)
+      {
+      cout << "Please enter valid number of players(2-4): ";
+      cin >> playerAmount;
+      }
+      else
+        break;
+  }
   
-  cout >> "Settiny up deck";
-  cout >> "Shuffling deck"
-  cout >> "Dealing hands"
+  theBoard.deal();// Deals cards to both players
+  
+  cout << "Settiny up deck";
+  cout << "Shuffling deck";
+  cout << "Dealing hands";
   return 0; 
 }
