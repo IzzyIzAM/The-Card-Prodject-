71b63b594e1ea3240dc4ddfe1661ff886dc785d6
IzzyIzAM <104239828+IzzyIzAM@users.noreply.github.com>
2022-05-06 20:02:56 -0500

    Update deck.h
    Blaaaaa

--- deck.h
+++ deck.h
@@ -1,1 +1,118 @@
 
+#ifndef DECK_H
+#define DECK_H
+
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+class card
+{
+public:
+	card() {
+		currentCard = 0;
+	}
+
+	card(int val) {
+		currentCard = val;
+	}
+
+	void setCard(card val) { currentCard = val.cardType(); }
+
+	void setCard(int val) { currentCard = val; }
+
+	int valueofcard() { return value[currentCard]; }
+
+	string visual() { return vis[currentCard]; }
+
+	int cardType() { return currentCard; }
+
+	void print() { cout << "[" << vis[currentCard] << "]"; }
+private:
+	string vis[16] = { "-3","A","2","3","4","5","6","7","8","9","10","J","Q","K","JK"," " };
+	int value[16] = { 100,1,2,3,4,5,6,7,8,9,10,10,0,-2,0 };
+	int currentCard;
+
+};
+
+class deckOfCards {
+private:
+	card* deck;
+	int numdeck, totalcards, index; //store the number of decks 2 for every 2 players, stors total number of cards, stores the index of the card
+	card discard; // discarded card
+
+	void reset() {
+		index = 0;
+		for (int k = 1; k <= numdeck; k++) { //for the number of decks
+			for (int j = 1; j <= 4; j++) { // for each suit
+				for (int i = 1; i <= 13; i++) { // for each card
+					deck[index].setCard(i);
+					index++;
+				}
+			}
+		}
+
+		for (int k = 1; k <= numdeck; k++) { //for the number of decks
+			for (int j = 1; j <= 2; j++) {
+				deck[index].setCard(14);
+				index++;
+			}
+		}
+
+		index = 0;
+	}
+
+public:
+	deckOfCards(int num = 1) {
+
+		totalcards = 52 * numdeck;
+		deck = new card[totalcards];
+		index = 0;
+
+		//set all cards to default
+		for (int t = 0; t < totalcards; t++) {
+			deck[t].setCard(0);
+		}
+
+		//maksure we have enough and the proper cads
+		reset();
+		randomize();
+
+	}
+
+	void randomize() {
+		for (int i = 0; i < totalcards; i++) {
+
+			//gets random place to swap with
+			int j = rand() % totalcards;
+
+			//swap places 
+			int temp = deck[i].cardType();
+			deck[i].setCard(deck[j].cardType());
+			deck[j].setCard(temp);
+		}
+		index = 0;
+	}
+
+	card draw() {
+		int i = index;
+		index++;
+		return deck[i];
+	}
+
+	void setDiscard(card car) {
+		discard.setCard(car);
+	}
+
+	card getDiscard() {
+		return discard;
+	}
+
+};
+
+
+
+#endif
