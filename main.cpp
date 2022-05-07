72013f2d05ef58852012ac1e9ef6640d919b5a2d
NightOfDevils <44612725+NightOfDevils@users.noreply.github.com>
2022-05-06 21:29:59 -0500

    Update main.cpp
    Added game set up and player turn

--- main.cpp
+++ main.cpp
@@ -7,67 +7,102 @@ using namespace std;
 
 int main()
 {
-  board theBoard(2);
-  deckOfCards decks;
-  card theCard;
-  int currentTurn = 1;
-  bool gameOn = true;
-  int playerAmount;
-  int row;
-  int column;
-  int randRow;
-  int randCol
-  bool isPlayer = true;
-  
-  cout << "**Setting Board and Hands**" << endl;
-  cout << endl;
+	board theBoard(2);
+  	deckOfCards decks;
+  	card theCard;
+  	int currentTurn = 0;
+  	bool gameOn = true;
+  	int playerAmount;
+  	int row;
+  	int column;
+  	int randRow;
+  	int randCol
+  	bool isPlayer = true;
 
-  deckOfCards theDeck; // Creates the Deck  
   
-  theBoard.deal(); // Deals cards to both players
+  	cout << "**Setting Board and Hands**" << endl;
+  	cout << endl;
+
+  	deckOfCards theDeck; // Creates the Deck  
+  
+  	theBoard.deal(); // Deals cards to both players
   
   
-  cout << "Please enter the card you would like to flip in row 1(1-3): ";
-  cin >> column;
-  theBoard.players[0].flip(1, column); // Flips players first chosen card
+  	cout << "Please enter the card you would like to flip in row 1(1-3): ";
+  	cin >> column;
+  	theBoard.players[0].flip(1, column); // Flips players first chosen card
   
-  cout << "Please enter the card you would like to flip in row 2(1-3): ";
-  cin >> column; 
-  theBoard.players[0].flip(2, column); // Flips players second chosen card
+  	cout << "Please enter the card you would like to flip in row 2(1-3): ";
+  	cin >> column; 
+  	theBoard.players[0].flip(2, column); // Flips players second chosen card
   
-  theBoard.players[0].flip(1, rand() % 3 + 1); // Flips bots first card
-  theBoard.players[0].flip(2, rand() % 3 + 1); // Flips bots second card
+  	theBoard.players[0].flip(1, rand() % 3 + 1); // Flips bots first card
+  	theBoard.players[0].flip(2, rand() % 3 + 1); // Flips bots second card
   
-  decks.setDiscard(deck.draw()); // Sets top of discard pile
+  	decks.setDiscard(deck.draw()); // Sets top of discard pile
   
-  cout << endl;
-  cout << "**Starting game**" << endl;
-  cout << endl;
+  	cout << endl;
+  	cout << "**Starting game**" << endl;
+  	cout << endl;
   
- while(gameOn) // Starts the game
- {
-	 int whoseTurn = currentTurn % theBoard.getNumPlayers();
-	 if(whoseTurn == 0)
-	 {
+ 	while(gameOn) // Starts the game
+ 	{
+		int whoseTurn = currentTurn % theBoard.getNumPlayers();
+		if(whoseTurn == 0)
+		{
+			cout << "**Player turn**" << endl;
 		 
-		 cout << "**Player turn**" << endl;
-		 
-     // display available card from discard
-		 theCard = decks.getDiscard();
-		 cout << "Top of Discard Pile: ";
-		 theCard.print(); // Prints the top of discard pile
+     			// display available card from discard
+			theCard = decks.getDiscard();
+			cout << "Top of Discard Pile: ";
+			theCard.print(); // Prints the top of discard pile
 
-     // displayer both "player's" hands
-     cout << "\n\nThe Player's Hand:\n";
-     theBoard.players[0].print();
-     cout << "\n\nOpponent's Hand:\n";
-     theBoard.players[1].print();
+     			// displayer both "player's" hands
+     			cout << "\n\nThe Player's Hand:\n";
+     			theBoard.players[0].print();
+     			cout << "\n\nOpponent's Hand:\n";
+     			theBoard.players[1].print();
 
-     // present play options
-	 }
-	 
- }
+     			// present play options
+			cout << "1. Pass" << endl;
+			cout << "2. Take From Deck" << endl;
+			cout << "3. Take From Discard" << endl;
+			cout << "Please enter what you would like to do: ";
+			cin >> playerChoice;
+			
+			while(true)
+			{
+				if(playerChoice < 1 || playerChoice > 3)
+				{
+					cout << "Please enter a valid choice: ";
+					cin >> playerChoice;
+				}
+			}
+			
+			if(playerChoice == 1)
+			{
+				theBoard.pass();
+			}
+			else if(playerChoice == 2)
+			{
+				theBoard.takeFromDeck();	
+			}
+			else if(playerChoice == 3)
+			{
+				theBoard.takeFromDiscard();	
+			}
+			
+			if(isAllFaceUp = true)
+			{
+				gameOn = false;
+			}
+			else
+			{
+				currentTurn++;	
+			}
+		}
+ 	}
  
 
-  return 0; 
+  	return 0; 
 }
